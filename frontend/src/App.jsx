15fbdd7aafc327a19463c6604dcbe84e76db2bee
Ronald Chiagozie <gronaldchia@gmail.com>
2025-11-11 16:39:21 -0800

    updated review

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/landing-page';
 import AboutPage from './components/about-us';
-import Project from './components/project';
-import Team from './components/Team';
-import Contact from './components/contact';
 import ProjectsPage from './components/project';
 import TeamPage from './components/Team';
 import ContactPage from './components/contact';
@@ -13,14 +9,14 @@ import ContactPage from './components/contact';
 const App = () => {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<AboutPage/>} />
-      <Route path="/projects" element={<ProjectsPage/>} />
-      <Route path="/team" element={<TeamPage/>} />
-      <Route path="/contact" element={<ContactPage/>} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
   );
 };
 
